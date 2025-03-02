96adc48ea1963f85c2264d5ec38850fe08490c14
Shaman3223 <Shashua321@gmail.com>
2025-03-01 20:01:01 -0500

    homework1

---
+++ Unit1/Unit1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Unit1
+{
+    internal class Program
+    {
+        
+        
+        
+        public static void Main(string[] args)
+        {
+            int maxSoda = 100;
+            int maxChips = 40;
+            int maxCandy = 60;
+            
+            int minSoda = 60;
+            int minChips = 20;
+            int minCandy = 40;
+            
+            maxSoda = ReturnMaxStock("Soda", stockvarmax: maxSoda, stockvarmin: minSoda);
+            maxCandy = ReturnMaxStock(stockname: "Candy", stockvarmax: maxCandy, stockvarmin: minCandy);
+            maxChips = ReturnMaxStock(stockname: "Chips", stockvarmax: maxChips, stockvarmin: minChips);
+            
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+            if (maxCandy <= minCandy) { Console.WriteLine("Before you leave work today, call Hershey. We need more candy"); }
+            if (maxChips <= minChips) { Console.WriteLine("Before you leave work today, call Lays. We need more chips"); }
+            if (maxSoda <= minSoda) { Console.WriteLine("Before you leave work today, call Pepsi. We need more pop");}
+        }
+
+        public static int ReturnMaxStock(string stockname, int stockvarmax, int stockvarmin)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("how many " + stockname + "(s) have been purchased?");
+            
+            int currentStock = Convert.ToInt32(Console.ReadLine());
+            if (currentStock >= stockvarmax)
+            {
+                Console.Error.WriteLine("impossible try again! >:(");
+                return stockvarmax;
+            }
+            else if ((stockvarmax - currentStock) < stockvarmin)
+            {
+                Console.WriteLine("We need to restock!!! " + (stockvarmax - currentStock) + stockname + "(s) is left");
+                return (stockvarmax - currentStock);
+            }
+            else
+            {
+                Console.WriteLine("No need for alarm, we have enough " + stockname + "(" + (stockvarmax - currentStock) + ")");
+                return (stockvarmax - currentStock);
+            }
+        }
+        
+    }
+}
